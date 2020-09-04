33f0420c3b9cdb929721043df576321e18a58bd4
=Erick Damián <=>
2020-09-04 15:34:45 -0600

    use axios all to make several requests

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const characterService = require('../services/characterService');
 const nitService = require('../services/nitService');
 const utils = require('../utils/utils');
@@ -7,25 +8,22 @@ exports.getById = async (req, res) => {
     const nit = utils.getNits(index);
     const { id } = req.params;
 
-    characterService.findCharacter(id)
-        .then((characterResponse) => {
+    const characterRequest = characterService.findCharacter(id);
+    const nitRequest = nitService.findByNit(nit);
+
+    axios.all([characterRequest, nitRequest]).then(
+        axios.spread((...responses) => {
             const person = {};
-            person.nombre = characterResponse.data.name;
-            person.estado = characterResponse.data.status;
-            nitService.findByNit(nit)
-                .then((nitResponse) => {
-                    person.nit = nitResponse.data.nit;
-                    res.send(person);
-                })
-                .catch((err) => {
-                    res
-                        .status(500)
-                        .send({ message: err.message });
-                });
+            const characterResponses = responses[0].data;
+            const nitResponse = responses[1].data;
+
+            person.nombre = characterResponses.name;
+            person.estado = characterResponses.status;
+            person.nit = nitResponse.nit;
+            res.send(person);
         })
-        .catch((err) => {
-            res
-                .status(500)
-                .send({ message: err.message });
+    )
+        .catch((error) => {
+            res.status(500).send({ message: error.message });
         });
 };
